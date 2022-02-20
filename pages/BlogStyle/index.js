6a7e6a6fd00e6a77f4dadd67e7edd50292c4f980
Shubham Chavan <shubhamchavan4554@gmail.com>
2022-02-20 13:41:02 +0530

    updated meta tag

--- pages/BlogStyle/index.js
+++ pages/BlogStyle/index.js
@@ -19,12 +19,12 @@ function BlogStyle({ blogsData }) {
                 <meta name="description" content={blogsMeta.description} />
                 <meta name="robots" content="index, follow, max-snippet:-1, max-video-preview:-1, max-image-preview:large"></meta>
                 <link rel="icon" href="/sage.jpg" />
-                <link rel="canonical" href={`https://blindoncloud.com/blog/${blogsMeta.route}`} />
+                <link rel="canonical" href={`https://blindoncloud.com/blog/aws/${blogsMeta.route}`} />
                 <meta property="og:locale" content="en_US"></meta>
                 <meta property="og:type" content="website"></meta>
                 <meta property="og:title" content={`${blogsMeta.route} - blindoncloud.com`}></meta>
                 <meta property="og:description" content={blogsMeta.description}></meta>
-                <meta property="og:url" content={`https://blindoncloud.com/blog/${blogsMeta.route}`}></meta>
+                <meta property="og:url" content={`https://blindoncloud.com/blog/aws/${blogsMeta.route}`}></meta>
                 <meta property="og:site_name" content="blindoncloud.com"></meta>
                 <meta name="author" content="Shubham Chavan" />
                 <meta property="og:image" content={`https://cloud-tech.s3.ap-south-1.amazonaws.com/Images/${blogsMeta.ogImage}`} />
